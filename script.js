7cf37325db0c57682127c91e3f5f1f2111a9a47f
lipskij <emil.lipskij@gmail.com>
2020-07-22 19:46:37 +0300

    home arrow

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const sections = document.querySelectorAll('section');
 const buble = document.querySelector('.buble');
+const arrow = document.querySelector('.arrows');
+
 
 const options = { // nav scrooling indicator
   threshold: 0.8
