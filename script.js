4bf113e637a958ff74d9e92aa8b7422f4e3a788b
lipskij <emil.lipskij@gmail.com>
2020-07-27 21:44:27 +0300

    refactor

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-const sections = document.querySelectorAll('section');
-const buble = document.querySelector('.buble');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-const options = { // nav scrooling indicator
-  threshold: 0.8
-};
-
-let observer = new IntersectionObserver(navCheck, options);
-
-function navCheck(entries) {
-  entries.forEach(entry => {
-    const className = entry.target.className;
-    const activeAnchor = document.querySelector(`[data-page=${className}]`);
-    const coords = activeAnchor.getBoundingClientRect();
-    const directions = {
-      height: coords.height,
-      width: coords.width,
-      left: coords.left
-    };
-    if (entry.isIntersecting) {
-      buble.style.setProperty('left', `${directions.left}px`);
-      buble.style.setProperty('width', `${directions.width}px`);
-      buble.style.setProperty('height', `${directions.height}px`);
-    }
+hamburger.addEventListener("click", () => {
+  navLinks.classList.add("open");
+  links.forEach(link => {
+    link.classList.add("fade");
   });
-}
-
-sections.forEach(section => {
-  observer.observe(section);
 });
 
-
-// add animation on crool down to animate text
+navLinks.addEventListener('click', () => {
+  const sepLink = navLinks;
+  sepLink.classList.remove("open")
+})
