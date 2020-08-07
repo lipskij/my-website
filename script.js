d7510a93c77d42c2e70d299c773ac3875a226637
lipskij <emil.lipskij@gmail.com>
2020-08-07 17:42:06 +0300

    dark-mode

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
-const darkTheme = document.querySelector('.toggle');
+const darkTheme = document.querySelector(".toggle");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.add("open");
-  links.forEach(link => {
+  links.forEach((link) => {
     link.classList.add("fade");
   });
 });
 
-navLinks.addEventListener('click', () => {
+navLinks.addEventListener("click", () => {
   const sepLink = navLinks;
-  sepLink.classList.remove("open")
-})
+  sepLink.classList.remove("open");
+});
 
-darkTheme.addEventListener('click', () => {
-  darkTheme.classList.toggle('focus');
-  document.body.classList.toggle('dark');
+darkTheme.addEventListener("click", () => {
+  darkTheme.classList.toggle("focus");
+  document.body.classList.toggle("dark");
 });
